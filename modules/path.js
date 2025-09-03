c64b406b2122e0683359a1397ecefc8e5875419f
Kauan Constancio <kauanconstancio13@gmail.com>
2025-09-03 13:48:32 -0300

    Introdução ao NodeJS

---
+++ modules/path.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+// Basename
+// Apenas o nome do arquivo atual
+
+// console.log(path.basename(__filename));
+
+// // Nome do diretório atual
+// console.log(path.dirname(__filename));
+
+// // Extensão do arquivo
+// console.log(path.extname(__filename));
+
+// // Criar objeto path
+// console.log(path.parse(__filename));
+
+// //Juntar caminhos de arquivos
+
+// console.log(path.join(__dirname, 'teste'));
